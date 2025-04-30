39531be99d03ac304fe075f1a3b0e73a40758feb
Naokichi55 <ibosanganba@gmail.com>
2025-04-30 15:07:47 +0900

    update:rubocop規約違反を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :configure_permitted_parameters, if: :devise_controller?#ユーザー登録時に名前を登録できるように追記
+  before_action :configure_permitted_parameters, if: :devise_controller?# ユーザー登録時に名前を登録できるように追記
   allow_browser versions: :modern
 
-  def configure_permitted_parameters #ユーザー登録時に名前を登録できるように定義
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  def configure_permitted_parameters # ユーザー登録時に名前を登録できるように定義
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :user_name ])
   end
 
   private
@@ -12,5 +12,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
-end  
-
+end
