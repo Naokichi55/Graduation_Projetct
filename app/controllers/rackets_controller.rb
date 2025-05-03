d7b28c3820d04dcb4d07325facc8db4d7255985d
Naokichi55 <ibosanganba@gmail.com>
2025-05-03 11:31:44 +0900

    add:ラケット投稿機能を実装

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -6,4 +6,19 @@ class RacketsController < ApplicationController
   def new
     @racket = Racket.new
   end
+
+  def create
+    @racket = current_user.rackets.build(racket_params)
+    if @racket.save
+      redirect_to rackets_path, success: t('defaults.flash_message.created', item: Racket.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Racket.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def racket_params
+    params.require(:racket).permit(:product_name, :maker_name, :face_size, :main_string, :cross_string, :main_string_tension, :cross_string_tension,  :weight_position, :grip_size, :grip_tape, :body)
+  end
 end
