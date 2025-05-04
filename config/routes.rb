f6afcaffb041327284db73c9111d5a96f3b3e9ba
Naokichi55 <ibosanganba@gmail.com>
2025-05-04 18:30:46 +0900

    add:投稿削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "static_pages/top"
   devise_for :users
   # get "rackets/index"
-  resources :rackets, only: %i[ index new  create show show edit update ]
+  resources :rackets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
