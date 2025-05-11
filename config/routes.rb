3ed4a9f0f57c9ec27c10503aef4152476349139d
Naokichi55 <ibosanganba@gmail.com>
2025-05-11 14:42:21 +0900

    add:コメント機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get "static_pages/top"
   devise_for :users
   # get "rackets/index"
-  resources :rackets
+  resources :rackets do
+
+  # コメント機能のルーティングを設定
+    resources :comments, only: %i[create edit destroy], shallow:true
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
