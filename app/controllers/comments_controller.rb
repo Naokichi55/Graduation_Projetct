af83a7abc27108054a68e5c50ff428be203a802f
Naokichi55 <ibosanganba@gmail.com>
2025-05-11 16:42:11 +0900

    fix:エラー修正を実施しコメント投稿機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
 
-def create
-	comment = current_user.comments.build(comment_params)
+  def create
+    comment = current_user.comments.build(comment_params)
 
-	if comment.save
-		redirect_to racket.path(comment.racket), success: 'コメント投稿に成功しました'
-	else
-    redirect_to racket_path(comment.racket), danger: 'コメント投稿に失敗しました'
-	end
+    if comment.save
+      redirect_to racket_path(comment.racket), success: 'コメント投稿に成功しました'
+    else
+      redirect_to racket_path(comment.racket), danger: 'コメント投稿に失敗しました'
+    end
+  end
+  private
 
   def comment_params
-    prams.require(:comment).permit(:body).merge(racket_i: params[:racket_id])
+    params.require(:comment).permit(:body).merge(racket_id: params[:racket_id])
   end
 end
