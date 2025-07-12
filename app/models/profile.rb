5182d679e0f791906560e506ace7b0d09704cf6f
Naokichi55 <ibosanganba@gmail.com>
2025-07-12 11:09:54 +0900

    profilesテーブルのmigrationファイルの内容を記載

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,17 @@
 class Profile < ApplicationRecord
+  def change
+		create_table :profiles do |t|
+    t.belongs_to :users, index: {unique: true}, foreign_key :tre
+    t.string :racket_name
+    t.string :history
+    t.string :play_style
+    t.string :level
+		t.string :generation
+		t.string :gender
+		t.text :body
+
+	end
+  end
+
+
 end
