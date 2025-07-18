75517b38be391e4c2b3b7ae362c89976a4ccc9f5
Naokichi55 <ibosanganba@gmail.com>
2025-07-19 02:06:13 +0900

    add:Gemfileにenum_helpを追加、app/models/profile.rbにenumを追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,17 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  enum generation: {
+    under_10: 0
+    teens: 1
+    twenties: 2
+    thirties: 3
+    forties: 4
+    fifties: 5
+    sixties: 6
+    over_70: 7
+  }
+
+  enum  gender: { male: 1, female: 2, other: 3 }
+
 end
