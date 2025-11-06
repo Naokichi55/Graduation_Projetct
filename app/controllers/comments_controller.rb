84f9abb93345a2385cf2dbdc2044ecdea1727af9
Naokichi55 <ibosanganba@gmail.com>
2025-11-07 00:32:46 +0900

    feat:comments_controller.rbにブロードキャストの記述を記載

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,35 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to racket_path(comment.racket), success: "コメント投稿に成功しました"
+    @racket = Racket.find(params[:racket_id])
+    @comment = current_user.comments.build(comment_params)
+    @comment.user = current_user
+
+#commentの内容が保存された時の動作
+    if @comment.save
+      ActionCable.server.broadcast(
+        "racket_#{@racket.id}_comments",
+        {
+          comment: render_to_string(
+            partial: 'comments/comment',
+            locals: { comment: @comment }
+          )
+        }
+      )
+      respond_to do |format| #リクエストされる処理を分けるメソッド
+        format.turbo_stream do #turbo_streamのリクエストが来た際の処理
+          render turbo_stream: [
+          turbo_stream.prepend("table-comment", partial: "comments/comment", locals: { comment: @comment}),
+          turbo_stream.replace("comment-form", partial: "comments/form", locals: { comment: Comment.new, racket: @racket})
+            ]
+          end
+        format.html { redirect_to @racket}
+      end
     else
-      redirect_to racket_path(comment.racket), danger: "コメント投稿に失敗しました"
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("comment-form", partial: "comments/form", locals: { comment: @comment, racket: @racket })
+        end
+      end
     end
   end
 
