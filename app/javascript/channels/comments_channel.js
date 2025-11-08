79b01eca857e25b405e929f2edae933b7d7d2c51
Naokichi55 <ibosanganba@gmail.com>
2025-11-09 07:29:25 +0900

    add:app/javascript/channels/comments_channel.jsを編集後comments_channel.jsにチャンネルでのデータ受信時の動作を記載

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,15 +1,45 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentsChannel", {
+document.addEventListener('DOMContentLoaded', function() {
+  //constにて変数を定義
+  const racketElement = document.querySelector('[data-racket-id]');
+  const racketId = racketElement?.dataset.racketId;
+
+ console.log('Racket I:', racketId);
+ console.log('Racket Element:', racketElement);
+
+if(racketId) {
+  const subscription = consumer.subscriptions.create({
+  channel: "CommentsChannel",
+  racket_id: racketId
+ }, {
   connected() {
-    // Called when the subscription is ready for use on the server
+    console.log("Connected to comments channel for racket")
+    // サブスクリプションがサーバー上で使用可能になったときに呼び出される。
   },
-
+//接続が切断された時の処理。デバッグとし記録。
   disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log("Disconnected from comments channel")
+    // サブスクリプションがサーバー上で終了した時に呼び出される。
   },
-
+//データを受け取った時の処理について記載。
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+     console.log("Received data:", data); 
+     //　リアルタイムでコメントを追加
+     //table-commentをracket-idに変更すると投稿がワンクリックで2回コメントが投稿される。
+    const commentsContainer = document.getElementById('racket-id')
+    console.log("Comments container:", commentsContainer);
+
+    if(commentsContainer && data.comment) {
+      commentsContainer.insertAdjacentHTML('afterbegin', data.comment);
+      console.log("Comment add successfully")
+    }else{
+     console.error("Comments container not found or no comment data");
+    }
   }
+  });
+}else{
+  console.log("No racket ID found, skipping WebSocket connection");
+  }
+    //チャネルのWebSocketで受信データがあるときに呼び出される
 });
