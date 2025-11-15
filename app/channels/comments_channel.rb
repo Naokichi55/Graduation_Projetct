391134a01ef0b1c07c99a2b2036ca583b5570409
Naokichi55 <ibosanganba@gmail.com>
2025-11-15 16:24:23 +0900

    fix:app/channels/comments_channel.rbの内容を修正

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,6 +1,7 @@
 class CommentsChannel < ApplicationCable::Channel
+# 読み込むチャンネルを記載
   def subscribed
-    # stream_from "some_channel"
+    stream_from "racket_#{params[:racket_id]}_comments"
   end
 
   def unsubscribed
