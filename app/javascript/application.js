3810910feb57e968f9a48817d0dd8aa49a9c2a00
Naokichi55 <ibosanganba@gmail.com>
2025-12-04 07:08:20 +0900

    fix:app/javascript/application.jsを変更。@hotwired/turbo-railsをコメントアウト。action_cable使用する際にリロードしないと非同期処理されない事象、リロードを複数回実施すると2回コメントが投稿される事象の対応。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"  # action_cable使用する際にリロードしないと非同期処理されない事象、リロードを複数回実施すると2回コメントが投稿されるためturboをoffに変更しました。
 import "./controllers"
 import "./channels"
