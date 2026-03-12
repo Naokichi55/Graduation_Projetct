a755b50804e0a5b9ca8a3d4d41ba878904cf18cb
Naokichi55 <ibosanganba@gmail.com>
2026-03-12 20:35:38 +0900

    update:db/schema.rbをcommit忘れのためcommit

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_03_130355) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar"
     t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
   end
 
