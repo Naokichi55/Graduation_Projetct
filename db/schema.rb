fadb70d26a1ea0b607371c5165492b42127fc2bf
Naokichi55 <ibosanganba@gmail.com>
2026-04-25 10:29:13 +0900

    一意制約を記載し忘れていたため、== 20251203130355 AddSolidCable: reverting ==================================== == 20251203130355 AddSolidCable: reverted (0.0771s) ===========================を実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_24_114812) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_03_130355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,13 +50,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_24_114812) do
     t.datetime "updated_at", null: false
     t.index ["racket_id"], name: "index_comments_on_racket_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "racket_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
