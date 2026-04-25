adab0ad52c0b78c2d24734834ff5947c3afa9d23
Naokichi55 <ibosanganba@gmail.com>
2026-04-25 09:15:57 +0900

    add:app/controllers/likes_controller.rbに追記

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+ def create 
+	@link = current_user.likes,build(racket_id: params[:racket_id])
+	@likes.save
+	redirect_back(fallback_location: root_path)
+ end
+
+  def destroy
+		@like = current_user.likes.find_by(racket: params[:racket_id])
+		@like.destroy!
+		redirect_back(fallback_location: root_path)
+  end
+
 end
